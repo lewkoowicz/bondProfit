371a02095e5b08d47e6cfd209ea95bd21bc6a4fa
lewkoowicz <kajlew1@gmail.com>
2024-07-23 21:12:39 +0200

    formatted the code to be consistent across repositories

--- bondProfit-api/src/main/java/com/lewkowicz/bondProfit/Application.java
+++ bondProfit-api/src/main/java/com/lewkowicz/bondProfit/Application.java
@@ -1,13 +1,31 @@
 package com.lewkowicz.bondProfit;
 
+import lombok.NonNull;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(@NonNull CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("https://bondprofit.eu")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
     }
 
 }
--- bondProfit-api/src/main/java/com/lewkowicz/bondProfit/controller/BondController.java
+++ bondProfit-api/src/main/java/com/lewkowicz/bondProfit/controller/BondController.java
@@ -1,15 +1,14 @@
 package com.lewkowicz.bondProfit.controller;
 
-import com.lewkowicz.bondProfit.dto.Bond;
 import com.lewkowicz.bondProfit.dto.BondRequest;
 import com.lewkowicz.bondProfit.dto.BondResponse;
 import com.lewkowicz.bondProfit.service.BondService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-import java.time.LocalDate;
-import java.util.List;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/bonds")
@@ -22,12 +21,6 @@ public class BondController {
     public ResponseEntity<BondResponse> createBonds(@RequestBody BondRequest bondRequest) {
         BondResponse response = bondService.createBonds(bondRequest.getMonthlyInvestment(), bondRequest.getInvestmentYears(), bondRequest.isReinvest());
         return ResponseEntity.ok(response);
-    }
-
-    public ResponseEntity<Void> reinvestMaturedBonds(@RequestBody List<Bond> bonds, @RequestParam int investmentYears) {
-        LocalDate endDate = LocalDate.now().plusYears(investmentYears);
-        bondService.reinvestMaturedBonds(bonds, endDate);
-        return ResponseEntity.ok().build();
     }
 
 }
